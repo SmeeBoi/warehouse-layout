aab328ebb69531e7daad45b242f1d4395a2f3688
SmeeBoi <sameer.desai.boston@gmail.com>
2024-06-11 12:14:20 -0400

    basic page

---
+++ src/pages/warehouse/index.tsx
@@ -0,0 +1,14 @@
+// src/pages/index.tsx
+
+import React from 'react';
+
+const Home = () => {
+  return (
+    <div>
+      <h1>Warehouse Layout Application</h1>
+      <p>Welcome to the Warehouse Layout Application</p>
+    </div>
+  );
+};
+
+export default Home;
